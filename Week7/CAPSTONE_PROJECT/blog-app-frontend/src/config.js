2d2c6b901e40f6020cb99db2e26f90bdfdceecab
ConsoleOverload <122792646+ConsoleOverload@users.noreply.github.com>
2026-05-22 13:39:00 +0530

    Final commit

--- Week7/CAPSTONE_PROJECT/blog-app-frontend/src/config.js
+++ Week7/CAPSTONE_PROJECT/blog-app-frontend/src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://two4eg110a45-blog-app-1-01l1.onrender.com"; // or your backend URL
+const BASE_URL = "https://blog-app-jncj.onrender.com"; // or your backend URL
 
 export default BASE_URL;
